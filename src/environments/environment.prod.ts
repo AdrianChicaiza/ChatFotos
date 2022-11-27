f6c4d39f32d98a78cc542570f86fae664bfa21ed
Adrian Chicaiza <74798975+AdrianChicaiza@users.noreply.github.com>
2022-11-27 16:58:49 -0500

    Fotos y chat

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCPIQrWZSjmnz_WjkPDMU6Na8esCluiiP4",
+    authDomain: "chat-137e9.firebaseapp.com",
+    databaseURL: "https://chat-137e9-default-rtdb.firebaseio.com",
+    projectId: "chat-137e9",
+    storageBucket: "chat-137e9.appspot.com",
+    messagingSenderId: "902864596395",
+    appId: "1:902864596395:web:0e952edc495021851176ee",
+  },
+};
