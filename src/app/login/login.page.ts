d97619a7945fab5983c6489fe3c27345a8e7c226
Adrian Chicaiza <74798975+AdrianChicaiza@users.noreply.github.com>
2022-11-27 21:38:16 -0500

    Chat con Fotos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -43,12 +43,12 @@ export class LoginPage implements OnInit {
 
   validation_messages = {
     'email': [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'pattern', message: 'Please enter a valid email.' }
+      { type: 'required', message: 'Correo es requerido.' },
+      { type: 'pattern', message: 'Por favor ingresa un correo valido.' }
     ],
     'password': [
-      { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+      { type: 'required', message: 'Contraseña requerida.' },
+      { type: 'minlength', message: 'La contraseña debe de ser de mas de 5 caracteres.' }
     ]
   };
 
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -18,12 +18,12 @@ export class RegisterPage implements OnInit {
 
   validation_messages = {
     'email': [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'pattern', message: 'Enter a valid email.' }
+      { type: 'required', message: 'Correo es requerido.' },
+      { type: 'pattern', message: 'Ingrese un correo valido.' }
     ],
     'password': [
-      { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+      { type: 'required', message: 'Contraseña requerida.' },
+      { type: 'minlength', message: 'La contraseña debe de ser de mas de 5 caracteres.' }
     ]
   };
 
@@ -51,7 +51,7 @@ export class RegisterPage implements OnInit {
       .then(res => {
         console.log(res);
         this.errorMessage = "";
-        this.successMessage = "Your account has been created. Please log in.";
+        this.successMessage = "Tu cuenta ha sido creada. Ya puedes iniciar sesion";
       }, err => {
         console.log(err);
         this.errorMessage = err.message;
